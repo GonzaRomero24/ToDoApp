83830f3171ef4b20649dc9fa5d73b64be2c8c668
Gonzalo Romero <yayox588@gmail.com>
2024-10-12 00:30:27 -0300

    todo app ad Task

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,42 @@
 import { useState } from 'react'
 import {InputToDo} from './components/InputToDo'
 
+interface TaskInterface {
+  id: number;
+  description: string;
+  statusTask: string
+  finish: boolean;
+  date?: Date;
+}
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  let taskToDo = []
+  
+  const [tasksArray, setTTasksArray] = useState(taskToDo)
+
+  const addNewTask = (valueTask:string): void =>{
+    if(valueTask.length < 1 ) return
+    const TaskInterface = {
+      id: tasksArray.length + 1,
+      description: valueTask,
+      statusTask: "No Iniciado",
+      finish: false,
+      date: new Date()
+    }
+    setTTasksArray([...tasksArray,TaskInterface])
+  }
 
   return (
     <>
-      <main className="grid justify-items-center content-center bg-orange-200 max-h-full max-w-full ">
+      <main className="grid justify-items-center content-center  bg-orange-200 max-h-full max-w-full ">
         
-        <section className='grid grid-cols-2 grid-rows-2 gap-4'>
+        <section className='grid grid-cols-2 grid-rows-2 gap-4 '>
           <h1 className='col-span-2  grid justify-items-center'>ToDo App</h1>
-          <InputToDo />
+          <InputToDo addNewTask ={addNewTask} />
+        </section>
+        <section className=' bg-green-400 max-h-full max-w-full'>
+          {tasksArray.map(task => <h1 key={task.id}>{task.description}</h1>)}
         </section>
       </main>
 
--- src/components/InputToDo.tsx
+++ src/components/InputToDo.tsx
@@ -1,15 +1,29 @@
-import React from "react";
+import { useState } from "react";
+export const InputToDo = ({addNewTask}) => {
+ const [task, setTask] = useState("");
 
-export const InputToDo = () => {
+  const inputChange = (event: React.ChangeEvent<HTMLInputElement>) =>{
+    setTask(event.target.value);
+  }
+
+  const sendTask = (event: React.MouseEvent<HTMLButtonElement>) =>{
+    event.preventDefault()
+    addNewTask(task)
+    setTask("")
+  }
+
+
+  const newTask = (event: React.MouseEvent<HTMLButtonElement>): void =>{
+    
+  }
   return (
     <>
     <div className="col-span-2 row-start-2">
       <form className="grid grid-cols-2 grid-rows-1 gap-4 mb-3">
-        <input className="col-span-2 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline " placeholder="Ingrese la tarea"></input>
-        <button className="col-start-3 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Guardar Tarea</button>
+        <input onChange={inputChange} value={task} className="col-span-2 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline " placeholder="Ingrese la tarea"></input>
+        <button onClick={sendTask} className="col-start-3 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Guardar Tarea</button>
       </form>
     </div>
-      
     </>
   );
 };
